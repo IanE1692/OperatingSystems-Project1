d2b7b3d97e4ac756da6af1ba08ef6d2d4d59cb49
IanE1692 <127979786+IanE1692@users.noreply.github.com>
2024-04-04 17:47:20 -0400

    Update producer.c

--- producer.c
+++ producer.c
@@ -1,3 +1,6 @@
+// Ian Esselburn
+// OS Project 1
+// producer.c
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -10,14 +13,16 @@
 #include "table.h"
 
 int main(int argc, char* argv[]) {
-    srand(time(NULL)); // random seed for project
+    srand(time(NULL));
 
+    // create or open shared memory
     int shared = shm_open("/Shared_memory", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
     if (shared == -1) {
         perror("Error on shm_open()");
         return 1;
     }
 
+    // set the size of shared memory
     if (ftruncate(shared, sizeof(struct table)) == -1) {
         perror("Error on ftruncate()");
         return 1;
@@ -30,22 +35,27 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    // initialize semaphores
     sem_init(&producer->empty_spots, 1, MAX_TABLE_SIZE);
     sem_init(&producer->filled_spots, 1, 0);
 
-    while (1) {
+    // produce items and add them to the shared buffer
+    int turn = 0;
+    while(turn < iterations) {
         for (int i = 0; i < MAX_TABLE_SIZE; ++i) {
-            sem_wait(&producer->empty_spots); // Wait for empty spot
+            sem_wait(&producer->empty_spots);
         }
         for (int i = 0; i < MAX_TABLE_SIZE; ++i) {
             int item = rand() % 100 + 1;
             producer->shared_buffer[i] = item;
             printf("producer produced %d\n", item);
         }
-        sem_post(&producer->filled_spots); // Signal buffer is filled
-        usleep(500000); // Delay for 0.5 second
+        sem_post(&producer->filled_spots);
+        usleep(500000);
+        ++turn;
     }
 
+    // clean up semaphores
     sem_destroy(&producer->empty_spots);
     sem_destroy(&producer->filled_spots);
 
